dfea1269ba9a7b83609e47f948acd728e2ae7f72
songlu67006 <10367006@nhu.edu.tw>
2017-07-04 11:05:08 +0800

    Update Visualizing-1.js

--- Visualizing-1.js
+++ Visualizing-1.js
@@ -1,2 +1,23 @@
-link,geoserve,historical-moment-tensor-map,historical-seismicity-map,nearby-cities,origin,p-wave-travel-times,phase-data,scitech-link,tectonic-summary,"},"geometry":{"type":"Point","coordinates":[126.3832,5.9775,111.16]},"id":"usc000csx3"},
-{"type":"Feature","properties":{"mag":5.7,"place":"35km ESE of Ndoi Island, Fiji","time":1348175020,"tz":-720,"url":"http://earthquake.usgs.gov/earthquakes/eventpage/usc000csw5","felt":0,"cdi":1,"mmi":2,"alert":"green","status":"REVIEWED","tsunami":null,"sig":"500","net":"us","code":"c000csw5","ids":",usc000csw5,","sources":",us,","types":",dyfi,eq-location-map,geoserve,historical-moment-tensor-map,historical-seismicity-map,losspager,nearby-cities,origin,p-wave-travel-times,phase-data,scitech-link,shakemap,tectonic-summary,"},"geometry":{"type":"Point","coordinates":[120.463666,23.469418]},"id":"usc000csw5"});
+eqfeed_callback{
+  "type":"Feature","properties":{
+    "mag":5.7,
+    "place":"35km ESE of Ndoi Island, Fiji","time":1348175020,
+    "tz":-720,
+    "url":"http://earthquake.usgs.gov/earthquakes/eventpage/usc000csw5",
+    "felt":0,
+    "cdi":1,
+    "mmi":2,
+    "alert":"green",
+    "status":"REVIEWED",
+    "tsunami":null,
+    "sig":"500",
+    "net":"us",
+    "code":"c000csw5",
+    "ids":",usc000csw5,",
+    "sources":",us,",
+    "types":",dyfi,eq-location-map,geoserve,historical-moment-tensor-map,historical-seismicity-map,losspager,nearby-cities,origin,p-wave-travel-times,phase-data,scitech-link,shakemap,tectonic-summary,"
+  },
+    "geometry":{
+     "type":"Point","coordinates":[120.463666,23.469418]},"id":"usc000csw5"
+  }
+};
